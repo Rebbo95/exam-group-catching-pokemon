6e06bfed094713fca26e52d15e17c35aae50fb8a
Tivva34 <gemtime1991@gmail.com>
2025-02-05 12:55:16 +0100

    catch and release pokemon

--- scripts/script.js
+++ scripts/script.js
@@ -19,22 +19,72 @@ startForm.addEventListener("submit", function (event) {
     oGameData.trainerGender = document.querySelector("#gender").value;
     // try catch för validering
     try {
-    } catch (error) {}
+    } catch (error) { }
 
     //dölj starskärm visa spel
     document.querySelector("form-wrapper").classList.add("d-none");
-    gameField.classList.remove(".d-none");
+    gameField.classList.remove("d-none");
 
     startForm();
 });
 
 function startGame() {
-    gameMusic.play();
-    //Starta spel och tid
+    gameMusic.currentTime = 0;
+    // gameMusic.play(); kommentrade ut för ja tröttna på musiken
+
     oGameData.startTimeInMilliseconds();
     oGameData.nmbrOfCaughtPokemons = 0;
+    oGameData.caughtPokemons = [];
+
 }
 
+
+function catchPokemon(pokemon) {
+    let originalSrc = pokemon.getAttribute("data-original-src");
+
+    //kolla om pokemon redan är fångad
+    if (pokemon.getAttribute("data-caught")) {
+        //släpp pokemon fri och återställ tidigare pokemon bild
+        releasePokemon(pokemon);
+    } else {
+        //fånga pokemon och byt till pokeboll
+        pokemon.src = "assets/ball.webp";
+        pokemon.classList.add("caught");
+        pokemon.setAttribute("data-caught", "true");
+
+
+        //lägg till i listan om den inte redan finns 
+        if (!oGameData.caughtPokemons.includes(originalSrc)) {
+            oGameData.caughtPokemons.push(originalSrc);
+        }
+        //kontrollera om alla pokemons har fångats
+        if (oGameData.caughtPokemons.length === 10) {
+            endGame();
+        }
+
+    }
+}
+
+
+function releasePokemon(pokemon) {
+    let originalSrc = pokemon.getAttribute("data-original-src");
+
+    if (originalSrc) {
+        //sätt tillbaka orginal pokemon
+        pokemon.src = originalSrc;
+        pokemon.classList.remove("caught");
+        pokemon.removeArrtibute("data-caught");
+
+        //ta bort från fångad listan
+        const index = oGameData.caughtPokemons.indexOf(originalSrc);
+        if (index !== -1) {
+            oGameData.caughtPokemons.splice(index, 1);
+        }
+
+
+    }
+
+}
 //Spawn random pokemon
 // catch pokemon
 //release pokemon
